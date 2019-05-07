297d118300f7e4d59028d261a2d435c0a39d6aa9
leolima21 <50239670+leolima21@users.noreply.github.com>
2019-05-06 23:44:27 -0300

    Update cv2camera.py

--- cv2camera.py
+++ cv2camera.py
@@ -5,11 +5,11 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 while(True):
-  # Capture frame-by-frame
-  ret, frame = cap.read()
+	# Capture frame-by-frame
+	ret, frame = cap.read()
   
-  # Display the resulting frame
-  cv2.imshow('frame', frame)
-  k = cv2.waitKey(30)
-  if k > 0:
-    break
+	# Display the resulting frame
+	cv2.imshow('frame', frame)
+
+	if cv2.waitKey(1) == ord('q'):
+		break
